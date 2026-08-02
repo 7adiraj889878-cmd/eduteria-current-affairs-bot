2f17632ec7256148c8e5435b35b26ebf47348e4d
7adiraj889878-cmd <7adiraj889878@gmail.com>
2026-08-02 16:30:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,20 @@
+import os
+import asyncio
+
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🎉 Welcome to EDUTERIA Current Affairs Bot!")
+    await update.message.reply_text("✅ Welcome to EDUTERIA Current Affairs Bot!")
 
-app = Application.builder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+async def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
 
-print("Bot Started...")
-app.run_polling()
+    print("Bot Started...")
+    await app.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
